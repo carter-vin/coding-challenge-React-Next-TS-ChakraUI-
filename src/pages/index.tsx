99974e22e988b04bf9c7efe67e4799283cb5de5b
carter-vin <cartervincent684@gmail.com>
2022-06-23 20:02:02 +0545

    Setup: Project setup as defined on Readme

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Box } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <Box bg="tomato" w="100%" p={4} color="white">
+      Vincent Talysight
+    </Box>
+  );
+};
+
+export default Home;
