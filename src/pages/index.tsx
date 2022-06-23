03f8643ebc43eea2d515566893e37e50b8be9546
carter-vin <cartervincent684@gmail.com>
2022-06-23 20:24:44 +0545

    Feature:  Working on UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,59 @@
-import type { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
+import type { NextPage } from 'next';
+import {
+	HStack,
+	Box,
+	Text,
+	VStack,
+	Image,
+	UnorderedList,
+	ListItem,
+	Button,
+} from '@chakra-ui/react';
 
 const Home: NextPage = () => {
-  return (
-    <Box bg="tomato" w="100%" p={4} color="white">
-      Vincent Talysight
-    </Box>
-  );
+	const neighboutState = [];
+	return (
+		<VStack>
+			<VStack justifyContent="start" alignItems="start">
+				<VStack justifyContent="start" alignItems="start">
+					<Text fontSize="4xl" textTransform="capitalize">
+						BEST Sportsbook offers in ...
+					</Text>
+					<Text fontSize="2xl">Neighbour is 2234 miles away from you.</Text>
+				</VStack>
+				<VStack>
+					<HStack
+						justifyContent="center"
+						alignItems="center"
+						spacing={16}
+						border="2px"
+						borderColor="gray.200"
+						rounded="lg"
+						padding={4}
+					>
+						<Image
+							src="https://bit.ly/dan-abramov"
+							alt="Dan Abramov"
+							boxSize="80px"
+							objectFit="cover"
+						/>
+						<VStack justifyContent="start" alignItems="start" flex={1}>
+							<Text fontSize="3xl" textTransform="capitalize">
+								BEST Sportsbook offers in ...
+							</Text>
+							<UnorderedList>
+								<ListItem>Lorem ipsum dolor sit amet</ListItem>
+								<ListItem>Consectetur adipiscing elit</ListItem>
+							</UnorderedList>
+						</VStack>
+						<Button colorScheme="blue" size="lg" textTransform="capitalize">
+							GET Bonus
+						</Button>
+					</HStack>
+				</VStack>
+			</VStack>
+		</VStack>
+	);
 };
 
 export default Home;
